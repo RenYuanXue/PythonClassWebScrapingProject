6502a6088814bbdd31228bf01afdad5859fc2508
PiPyPie <windkun2015@gmail.com>
2026-04-15 19:55:07 -0400

    Update main.py

--- main.py
+++ main.py
@@ -13,10 +13,8 @@ response = requests.get(url)
 headers = {"User-Agent": "Mozilla/5.0"}
 parsed_content = html.fromstring(response.content)
 
-
 i = 1
 j = 1
-#
 try:
     while True:
         name_xpath = '//*[@id="filter-results"]/ul/li[' + str(i) + ']/product-card/div[3]/div[1]/div/p[1]/a'
@@ -34,20 +32,19 @@ try:
         j = j+1
 except IndexError as e:
         print(e)
-#
+
 for x in range(13):
-    price_xpath = '//*[@id="filter-results"]/ul/li[' + str(i+1) + ']/product-card//span[contains(@class,"price")]//span[last()]'
-    name_xpath = '/html/body/main/div[3]/div/custom-pagination/div[2]/ul/li[' + str(i+1) + ']/product-card/div[3]/div[1]/div/p[1]/a/text()'
-    link_xpath = '//*[@id="filter-results"]/ul/li[' + str(i+1) + ']//a[contains(@href,"/products/")]'
+    price_xpath = '//*[@id="filter-results"]/ul/li[' + str(x+1) + ']/product-card//span[contains(@class,"price")]//span[last()]'
+    link_xpath = '//*[@id="filter-results"]/ul/li[' + str(x+1) + ']//a[contains(@href,"/products/")]'
 
     price = parsed_content.xpath(price_xpath)
-    name = parsed_content.xpath(name_xpath)
+
     link = parsed_content.xpath(link_xpath)
 
-    product_name = name[0].strip()
+
     product_price = price[0].text_content().strip()
 
-    data.append([product_name, product_price])
+    data.append([product_price])
 
 
 
