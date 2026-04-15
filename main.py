9cb0499a62ea50c8932f80cb0e96fabe9b1b3c4f
PiPyPie <windkun2015@gmail.com>
2026-04-15 19:27:24 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,12 @@ import pandas as pd
 
 name_list = []
 review_list = []
+data = []
 
 # getting the URL
 url  = 'https://keychron.ca/collections/keychron-lemokey-series-keyboard-collection'
 response = requests.get(url)
+headers = {"User-Agent": "Mozilla/5.0"}
 parsed_content = html.fromstring(response.content)
 
 
@@ -32,11 +34,27 @@ try:
         j = j+1
 except IndexError as e:
         print(e)
+#   Jasper's loopy thingy
+for i in range(12):
+    price_xpath = '//*[@id="filter-results"]/ul/li[' + str(i+1) + ']/product-card//span[contains(@class,"price")]//span[last()]'
+    name_xpath = '/html/body/main/div[3]/div/custom-pagination/div[2]/ul/li[' + str(i+1) + ']/product-card/div[3]/div[1]/div/p[1]/a/text()'
+    link_xpath = '//*[@id="filter-results"]/ul/li[' + str(i+1) + ']//a[contains(@href,"/products/")]'
+
+    price = parsed_content.xpath(price_xpath)
+    name = parsed_content.xpath(name_xpath)
+    link = parsed_content.xpath(link_xpath)
+
+    product_name = name[0].strip()
+    product_price = price[0].text_content().strip()
+
+    data.append([product_name, product_price])
 
 
 
 # final CSV file
 c = pd.DataFrame({'name':name_list,
-                  'review':review_list
+                  'review':review_list,
+                  'prices':price,
+                  'link':link
                  })
 print(c.head)
