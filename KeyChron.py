653487565fb1841ec820b9589de19840647946f2
PiPyPie <windkun2015@gmail.com>
2026-04-29 19:32:00 -0400

    Update KeyChron.py

--- KeyChron.py
+++ KeyChron.py
@@ -1,34 +1,27 @@
 import requests
 from lxml import html
 import pandas as pd
-name_list = []
-review_list = []
+
 url  = 'https://keychron.ca/collections/keychron-lemokey-series-keyboard-collection'
 response = requests.get(url)
 parsed_content = html.fromstring(response.content)
 
 i = 1
 j = 1
-try:
-    while True:
-        name_xpath = '//*[@id="filter-results"]/ul/li[' + str(i) + ']/product-card/div[3]/div[1]/div/p[1]/a'
+def name_reviews():
+        try:
+                while True:
+                        name_xpath = '//*[@id="filter-results"]/ul/li[' + str(i) + ']/product-card/div[3]/div[1]/div/p[1]/a'
 
-        curr_name = parsed_content.xpath(name_xpath)[0].text
-        
-        name_list.append(curr_name)
-        i = i+1
+                        curr_name = parsed_content.xpath(name_xpath)[0].text
+                        
+                        i = i+1
 
-        review_xpath = '//*[@id="filter-results"]/ul/li[' + str(j) +']/product-card/div[3]/div[1]/div/div[1]/div[1]'
+                        review_xpath = '//*[@id="filter-results"]/ul/li[' + str(j) +']/product-card/div[3]/div[1]/div/div[1]/div[1]'
 
-        curr_review = parsed_content.xpath(review_xpath)[0].get('aria-label')
-        
-        review_list.append(curr_review)
-        j = j+1
-except IndexError as e:
-        print(e)
-
-
-c = pd.DataFrame({'name':name_list,
-                  'review':review_list
-                 })
-print(c.head)
+                        curr_review = parsed_content.xpath(review_xpath)[0].get('aria-label')
+                        
+                        j = j+1
+                        return curr_review, curr_name
+        except IndexError as e:
+                print(e)
