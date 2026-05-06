befccfb01d1c70cb27ff4b52232b44822f9b7e07
Comatose-rgb123 <caruny@gmail.com>
2026-05-06 13:17:22 -0400

    Prices update to main
    Updated prices to main

--- Prices.py
+++ Prices.py
@@ -4,29 +4,32 @@ import pandas as pd
 
 url = "https://keychron.ca/collections/keychron-lemokey-series-keyboard-collection"
 headers = {"User-Agent": "Mozilla/5.0"}
-data = []
 response = requests.get(url, headers=headers)
+price_list = []
 parsed_content = html.fromstring(response.content)
 
+base_url = "https://keychron.ca"
 
-for i in range(12):
-    price_xpath = '//*[@id="filter-results"]/ul/li[' + str(i+1) + ']/product-card//span[contains(@class,"price")]//span[last()]'
-    name_xpath = '/html/body/main/div[3]/div/custom-pagination/div[2]/ul/li[' + str(i+1) + ']/product-card/div[3]/div[1]/div/p[1]/a/text()'
-    link_xpath = '//*[@id="filter-results"]/ul/li[' + str(i+1) + ']//a[contains(@href,"/products/")]'
+def return_prices():
+    for i in range(20):
+        price_xpath = '//*[@id="filter-results"]/ul/li[' + str(i+1) + ']/product-card//span[contains(@class,"price")]//span[last()]'
+        name_xpath = '/html/body/main/div[3]/div/custom-pagination/div[2]/ul/li[' + str(i+1) + ']/product-card/div[3]/div[1]/div/p[1]/a/text()'
+        link_xpath = '//*[@id="filter-results"]/ul/li[' + str(i+1) + ']//a[contains(@href,"/products/")]'
 
-    price = parsed_content.xpath(price_xpath)
-    name = parsed_content.xpath(name_xpath)
-    link = parsed_content.xpath(link_xpath)
+        price = parsed_content.xpath(price_xpath)
+        name = parsed_content.xpath(name_xpath)
+        link = parsed_content.xpath(link_xpath)
 
-    product_name = name[0].strip()
-    product_price = price[0].text_content().strip()
+        if not price or not name or not link:
+            continue
 
-    data.append([product_name, product_price])
+        product_name = name[0].strip()
+        product_price = price[0].text_content().strip()
+        product_link = base_url + link[0].get("href")
 
-df = pd.DataFrame(data, columns=["Name", "Price"])
-print(df)
+        price_list.append(product_price)
 
-df.to_csv("keychron_products.csv", index=False)
-
+    print(price_list)
 
 
+return_prices()
--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 # required libraries
 import pandas as pd
 from KeyChron import name_reviews
+from Prices import return_prices
 
 reviews, names = name_reviews()
+prices = return_prices
 
 c = pd.DataFrame({'name':names,
                   'review':reviews,
+                  'prices':prices
                  })
 print(c.head)
