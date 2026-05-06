32c61db77311a73e1343fef6808e369c7e3e6d52
Kylegu <kztg2021@gmail.com>
2026-05-06 19:44:40 -0400

    Turned into color = ....
    yeah

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 # required libraries
 import pandas as pd
+from Colors import return_colors
 from KeyChron import name_reviews
 from Prices import return_prices
 
 reviews, names = name_reviews()
 prices = return_prices
-
+colors = return_colors()
 c = pd.DataFrame({'name':names,
                   'review':reviews,
-                  'prices':prices
+                  'prices':prices,
+                  'colors':colors
                  })
 print(c.head)
