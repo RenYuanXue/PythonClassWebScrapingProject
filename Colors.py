7247ff82043bea5aff9ced39486e560b0e72b663
PTX <peterxue2011@gmail.com>
2026-05-06 20:20:55 -0400

    Review and fixed errors

--- Colors.py
+++ Colors.py
@@ -1,35 +1,26 @@
 import requests
 from lxml import html
-import pandas as pd
 
 url = "https://keychron.ca/collections/keychron-lemokey-series-keyboard-collection"
 headers = {"User-Agent": "Mozilla/5.0"}
 response = requests.get(url, headers=headers)
-price_list = []
+color_list = []
 parsed_content = html.fromstring(response.content)
 
 base_url = "https://keychron.ca"
 
 def return_colors():
-    for i in range(20):
+    for i in range(14):
         color_xpath = '//*[@id="filter-results"]/ul/li[' + str(i+1) + ']/product-card/div[3]/div[1]/div/p[2]'
-        name_xpath = '/html/body/main/div[3]/div/custom-pagination/div[2]/ul/li[' + str(i+1) + ']/product-card/div[3]/div[1]/div/p[1]/a/text()'
-        link_xpath = '//*[@id="filter-results"]/ul/li[' + str(i+1) + ']//a[contains(@href,"/products/")]'
 
         color = parsed_content.xpath(color_xpath)
-        name = parsed_content.xpath(name_xpath)
-        link = parsed_content.xpath(link_xpath)
 
-        if not color or not name or not link:
+        if not color:
+            color_list.append("N/A")
             continue
 
-        product_name = name[0].strip()
         product_color = color[0].text_content().strip()
-        product_link = base_url + link[0].get("href")
 
-        price_list.append(product_color)
+        color_list.append(product_color)
 
-    print(price_list)
-
-
-return_colors()
+    return(color_list)
--- main.py
+++ main.py
@@ -5,11 +5,19 @@ from KeyChron import name_reviews
 from Prices import return_prices
 
 reviews, names = name_reviews()
-prices = return_prices
+prices = return_prices()
 colors = return_colors()
-c = pd.DataFrame({'name':names,
-                  'review':reviews,
-                  'prices':prices,
-                  'colors':colors
-                 })
-print(c.head)
+
+if __name__ == "__main__":
+
+    reviews, names = name_reviews()
+    prices = return_prices()
+    colors = return_colors()
+
+    c = pd.DataFrame({'name':names,
+                    'review':reviews,
+                    'prices':prices,
+                    'colors':colors
+                    })
+
+    c.to_csv('keychron.csv', index=False)
